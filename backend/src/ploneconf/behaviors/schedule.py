ab55279ad8d71968611055c1802e8d36ab100076
Érico Andrei <ericof@gmail.com>
2024-09-19 09:11:13 -0300

    Update packages, prepare schedule

--- backend/src/ploneconf/behaviors/schedule.py
+++ backend/src/ploneconf/behaviors/schedule.py
@@ -1,5 +1,6 @@
 from plone.app.event.base import default_timezone
 from plone.app.event.dx.behaviors import StartBeforeEnd
+from plone.app.multilingual.dx import directives as ml_directives
 from plone.app.z3cform.widget import DatetimeFieldWidget
 from plone.autoform import directives
 from plone.autoform.interfaces import IFormFieldProvider
@@ -17,8 +18,9 @@ class IScheduleSlot(model.Schema):
     model.fieldset(
         "schedule",
         label=_("Schedule"),
-        fields=["start", "end", "track"],
+        fields=["start", "end", "room", "track"],
     )
+    ml_directives.languageindependent(("start", "end", "room", "track"))
 
     start = schema.Datetime(title=_("Start time"), required=False)
     directives.widget(
@@ -37,6 +39,15 @@ class IScheduleSlot(model.Schema):
     def validate_start_end(data):
         if data.start and data.end and data.start > data.end:
             raise StartBeforeEnd(_("End time must be after start time."))
+
+    room = schema.Set(
+        title=_("Room"),
+        description=_(""),
+        value_type=schema.Choice(
+            vocabulary="ploneconf.vocabularies.slot_rooms",
+        ),
+        required=False,
+    )
 
     track = schema.Set(
         title=_("Track"),
--- backend/src/ploneconf/vocabularies/talk.py
+++ backend/src/ploneconf/vocabularies/talk.py
@@ -9,7 +9,7 @@ def talk_duration(context):
     """Available Talk Duration."""
     return SimpleVocabulary(
         [
-            SimpleTerm(value="LongTalk", title="Long Talk"),
-            SimpleTerm(value="ShortTalk", title="Short Talk"),
+            SimpleTerm(value="LongTalk", title="40 minutes"),
+            SimpleTerm(value="ShortTalk", title="30 minutes"),
         ]
     )
